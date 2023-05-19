ba3f6fa72e999b4e277b74ecac13892aefd66eba
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-05-19 10:12:05 -0500

    initial release of step functions

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "role_name" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role#name"
   value       = aws_iam_role.this.name
 }
+
+output "role_arn" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role#arn"
+  value       = aws_iam_role.this.arn
+}
