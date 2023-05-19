4b2b648a4d5ad743091ecfe4c8e1a2f44653a0e7
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-05-19 12:46:10 -0500

    adding additonal policy for logs

--- iam.tf
+++ iam.tf
@@ -1,9 +1,9 @@
 resource "aws_iam_role" "this" {
   name               = "${var.git}-state-machine-role"
-  assume_role_policy = data.aws_iam_policy_document.this.json
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
-data "aws_iam_policy_document" "this" {
+data "aws_iam_policy_document" "assume_role" {
   statement {
     effect  = "Allow"
     actions = ["sts:AssumeRole"]
@@ -13,10 +13,24 @@ data "aws_iam_policy_document" "this" {
       type        = "Service"
     }
   }
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  policy_arn = aws_iam_policy.this.arn
+  role       = aws_iam_role.this.name
+}
+
+data "aws_iam_policy_document" "this" {
   statement {
     actions = [
-      "logs:CreateLogStream",
-      "logs:PutLogEvents",
+      "logs:CreateLogDelivery",
+      "logs:GetLogDelivery",
+      "logs:UpdateLogDelivery",
+      "logs:DeleteLogDelivery",
+      "logs:ListLogDeliveries",
+      "logs:PutResourcePolicy",
+      "logs:DescribeResourcePolicies",
+      "logs:DescribeLogGroups"
     ]
     resources = ["*"]
   }
