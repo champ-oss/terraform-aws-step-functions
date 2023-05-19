df2ec3adcec0ae0a9a80081601dfa9e91c66b79a
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-05-19 10:09:45 -0500

    initial release of step functions

--- iam.tf
+++ iam.tf
@@ -14,13 +14,4 @@ data "aws_iam_policy_document" "this" {
     }
   }
 
-  statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      identifiers = ["events.amazonaws.com"]
-      type        = "Service"
-    }
-  }
 }
