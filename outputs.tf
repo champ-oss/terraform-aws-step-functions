e8278afe0b2c3fd725847d508192d20c9f06d3cb
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-05-22 13:35:44 -0500

    adding sfn arn for output var

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "role_arn" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role#arn"
   value       = aws_iam_role.this.arn
 }
+
+output "sfn_arn" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/sfn_state_machine#arn"
+  value       = aws_sfn_state_machine.this.arn
+}
