bb18a5bda87231699a65eb9d0bcad0d8a8121399
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-07-20 13:58:11 -0500

    output var for sfn name

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "sfn_arn" {
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/sfn_state_machine#arn"
   value       = aws_sfn_state_machine.this.arn
 }
+
+output "sfn_name" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/sfn_state_machine#name"
+  value       = aws_sfn_state_machine.this.name
+}
